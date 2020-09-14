fc5b9a4d4916aba7e380bdb0fcea6769706d3cc8
Ishaan Jain <ishaan160799@gmail.com>
2020-09-14 14:45:51 +0530

    first note to firestore done

--- app/src/main/java/com/ironclad/firestoredemo/MainActivity.kt
+++ app/src/main/java/com/ironclad/firestoredemo/MainActivity.kt
@@ -1,11 +1,44 @@
 package com.ironclad.firestoredemo
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private val TAG = "PUI"
+    private val keyTitle = "title"
+    private val keyDescription = "description"
+
+    val db by lazy {
+        Firebase.firestore
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnSubmit.setOnClickListener {
+            val title = etTitle.text.toString()
+            val description = etDescription.text.toString()
+
+            val message = hashMapOf(
+                keyTitle to title,
+                keyDescription to description
+            )
+
+            db.collection("Notebook")
+                .document("First Note")
+                .set(message)
+                .addOnSuccessListener {
+                    Log.d(TAG, "Document added with id:$it.id")
+                }
+                .addOnFailureListener {
+                    Log.d(TAG, "Document Failed ${it.stackTrace}")
+                }
+        }
     }
 }
