e88bf047c7e28b43a11a4cf6a933f8612351ab19
Ishaan Jain <ishaan160799@gmail.com>
2020-09-14 16:19:25 +0530

    data loaded from firestore

--- app/src/main/java/com/ironclad/firestoredemo/MainActivity.kt
+++ app/src/main/java/com/ironclad/firestoredemo/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.ironclad.firestoredemo
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.firestore.ListenerRegistration
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_main.*
@@ -13,10 +15,13 @@ class MainActivity : AppCompatActivity() {
     private val keyTitle = "title"
     private val keyDescription = "description"
 
-    val db by lazy {
+    private val db by lazy {
         Firebase.firestore
     }
 
+    private lateinit var noteListener: ListenerRegistration
+
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -40,5 +45,42 @@ class MainActivity : AppCompatActivity() {
                     Log.d(TAG, "Document Failed ${it.stackTrace}")
                 }
         }
+
+        btnLoad.setOnClickListener {
+            db.collection("Notebook")
+                .document("First Note")
+                .get()
+                .addOnSuccessListener {
+                    if (it.exists()) {
+                        val title = it.getString(keyTitle)
+                        val description = it.getString(keyDescription)
+
+                        tvShow.text = "Title: $title \n Description: $description"
+                    } else {
+                        Log.d(TAG, "Document doesn't exist!")
+                    }
+                }
+                .addOnFailureListener {
+                    Log.d(TAG, "Document failed: ${it.toString()}")
+                }
+        }
+    }
+
+    @SuppressLint("SetTextI18n")
+    override fun onStart() {
+        super.onStart()
+        noteListener = db.collection("Notebook")
+            .document("First Note").addSnapshotListener { value, error ->
+                if (error != null) {
+                    Log.d(TAG, "Document error: ${error.toString()}")
+                }
+
+                if (value!!.exists()) {
+                    val title = value.getString(keyTitle)
+                    val description = value.getString(keyDescription)
+
+                    tvShow.text = "Title: $title \n Description: $description"
+                }
+            }
     }
 }
